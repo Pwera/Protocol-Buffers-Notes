4cae7cf377b92c28ef507f37113ce29214f0c340
Piotr Wera <piotr.wera@vp.pl>
2019-08-26 21:18:06 +0200

    update protoc-gen-go script

--- src/main/proto/script.sh
+++ src/main/proto/script.sh
@@ -2,7 +2,7 @@ FILES=*.proto
 for f in $FILES
 do 
   PB=$(echo $f | cut -d'/' -f4-)
-  protoc --plugin=$GOBIN/protoc-gen-go $f --go_out=../go/pb
+  protoc --plugin=/usr/local/bin/protoc-gen-go $f --go_out=../go/pb
   protoc $f --cpp_out=../generated/cpp
   protoc $f --java_out=../java
 done
